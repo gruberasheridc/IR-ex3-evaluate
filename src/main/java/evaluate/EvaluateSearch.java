57d158a4ffcb1fa012ad15b5ad808317045a88dc
Asher Gruber <asher@legolas-media.com>
2015-08-31 21:58:06 +0300

    Initial commit.

---
+++ src/main/java/evaluate/EvaluateSearch.java
@@ -0,0 +1,13 @@
+package evaluate;
+
+public class EvaluateSearch {
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Must accept two parameter: (1) Search output file. (2) truth.txt.");
+			return;
+		}
+		
+	}
+
+}
